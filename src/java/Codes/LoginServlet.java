e60a7f34f36e85315768263c29d17cbf809b835b
Soumyadeep Thakur <soumyadeep.thakur@gmail.com>
2016-11-05 23:21:11 +0530

    Update LoginServlet.java

--- src/java/Codes/LoginServlet.java
+++ src/java/Codes/LoginServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Codes;
 
 import java.security.NoSuchAlgorithmException;
@@ -10,20 +5,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author MANOSIJ
- */
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID=1L;//important line
     @Override
@@ -43,8 +31,8 @@ public class LoginServlet extends HttpServlet {
             password = hash(password);
         }
         catch (NoSuchAlgorithmException ex) {
-       Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-       }
+        	Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+       	}
         boolean val=isPresent(password,name);
         if(val){
             try {
@@ -53,8 +41,9 @@ public class LoginServlet extends HttpServlet {
                 out.print("<br />");
                 out.print("password - "+password);
                 out.print("</body></html>");
-            }catch(Exception e) {
-                out.close();
+            }
+	    catch(Exception e) {
+               	out.close();
              }
         }
         else{
@@ -81,32 +70,47 @@ public class LoginServlet extends HttpServlet {
         }
         return hashtext;
     }
-    public boolean isPresent(String passwrord,String name){
+    public boolean isPresent(String roll,String name){
         boolean flag=false;
         try{
             Class.forName("com.mysql.jdbc.Driver");
-				//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mysql","root","papan2202");
-				//Statement stmt = conn.createStatement();
+		//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mysql","root","papan2202");
+		//Statement stmt = conn.createStatement();
 
             System.out.println("Connection is being created");
             Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sis_db","root","abcdefgh");
-            //here sonoo is database name, root is username and password
-            System.out.println("Connection done");
-            java.sql.Statement stmt=myConn.createStatement();
-            System.out.println("query being done");
-            ResultSet rs=stmt.executeQuery("select id from sis_db.student_main where roll_no like "+name);
-            System.out.println("queried");
-            myConn.close();
-            System.out.println("Connection closed");
-            if(rs!=null) {
-                flag= true;
+            //System.out.println("Connection done");
+            Statement stmt=myConn.createStatement();
+	    String password="";
+	    int count=0;
+	    try
+            {
+                password = hash(password);
             }
-            else{
+            catch (NoSuchAlgorithmException ex) {
+        	Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+       	    }
+	    String sqlStr = "SELECT * FROM login_details WHERE roll_no like ";
+            sqlStr += "'" + name + "'";
+	    sqlStr += "AND pass_hash like '"+encr+"'";
+            //System.out.println("query being done");
+            ResultSet rs=stmt.executeQuery(sqlStr);
+            //System.out.println("queried");
+            myConn.close();
+            //System.out.println("Connection closed");
+	    while(rs.next())
+	    {
+		count++;
+	    }
+            if(count==0) {
                 flag=false;
             }
-        }catch(Exception e){
-
-            System.out.println(e);}
-        return flag;
+            else{
+                flag=true;
             }
+        }
+	catch(Exception e){
+	    System.out.println(e);}
+            return flag;
+        }
 }
