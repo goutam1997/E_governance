1b8832e28ae01951ef78199bff8402ea03fc6ecf
Soumyadeep Thakur <soumyadeep.thakur@gmail.com>
2016-12-24 14:08:04 +0530

    Update LoginServlet.java

--- src/java/Codes/LoginServlet.java
+++ src/java/Codes/LoginServlet.java
@@ -1,63 +1,100 @@
 package Codes;
 
 import java.security.NoSuchAlgorithmException;
-import java.io.IOException;
+import java.io.*;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet {
+/**
+ *
+ * @author SOUMYADEEP
+ */
+public class LoginServlet extends HttpServlet
+{
     private static final long serialVersionUID=1L;//important line
+	class Details
+	{
+		boolean flag;
+		int id;
+		String name,roll_no,user_type;
+		Details()
+		{
+			flag=false;
+			id=-1;
+			name="";
+			roll_no="";
+			user_type="";
+		}
+	}
     @Override
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException{
-        try {
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException
+	{
+        try 
+		{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             System.out.println("Driver Loaded");
-        } catch (Exception ex) {
+        }
+		catch (Exception ex) 
+		{
             System.out.println("SQL Driver not Found");
         }
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
-        String name=request.getParameter("username");
-        String password=request.getParameter("passwords");
+        String name=request.getParameter("uname");
+        String password=request.getParameter("rno");
         try
         {
             password = hash(password);
         }
-        catch (NoSuchAlgorithmException ex) {
-        	Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-       	}
-        boolean val=isPresent(password,name);
-        if(val){
-            try {
+        catch (NoSuchAlgorithmException ex) 
+		{
+			Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Details val=isPresent(password,name);
+        if(val.flag)
+		{
+			request.setAttribute("result",val.name);
+			getServletContext().getRequestDispatcher("/hello.jsp").forward(request, response);
+			/*
+            try 
+			{
                 out.print("<html><body>");
                 out.print("welcome "+name);
                 out.print("<br />");
                 out.print("password - "+password);
                 out.print("</body></html>");
             }
-	    catch(Exception e) {
-               	out.close();
-             }
+			catch(Exception e) 
+			{
+                out.close();
+            }
+			*/
         }
-        else{
-            try {
+        else
+		{
+            try 
+			{
                 out.print("<html><body>");
                 out.print("Incorrect credentials "+name);
                 out.print("<br />");
                 out.print("</body></html>");
-            }catch(Exception e) {
+            }
+			catch(Exception e) 
+			{
                 out.close();
             }
         }
     }
-    public String hash(String password) throws NoSuchAlgorithmException {
+    public String hash(String password) throws NoSuchAlgorithmException 
+	{
         MessageDigest m = MessageDigest.getInstance("MD5");
         m.reset();
         m.update(password.getBytes());
@@ -65,52 +102,57 @@ public class LoginServlet extends HttpServlet {
         BigInteger bigInt = new BigInteger(1,digest);
         String hashtext = bigInt.toString(16);
         // Now we need to zero pad it if you actually want the full 32 chars.
-        while(hashtext.length() < 32 ){
+        while(hashtext.length() < 32 )
+		{
           hashtext = "0"+hashtext;
         }
         return hashtext;
     }
-    public boolean isPresent(String roll,String name){
-        boolean flag=false;
-        try{
+    public Details isPresent(String passwrord,String name)
+	{
+        Details obj=new Details();
+        try
+		{
             Class.forName("com.mysql.jdbc.Driver");
-		//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mysql","root","papan2202");
-		//Statement stmt = conn.createStatement();
+				//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mysql","root","papan2202");
+				//Statement stmt = conn.createStatement();
 
             System.out.println("Connection is being created");
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sis_db","root","abcdefgh");
-            //System.out.println("Connection done");
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/sis_db","root","papan2202");
+            //here sonoo is database name, root is username and password
+            System.out.println("Connection done");
             Statement stmt=myConn.createStatement();
-	    String password="";
-	    int count=0;
-	    try
-            {
-                password = hash(password);
-            }
-            catch (NoSuchAlgorithmException ex) {
-        	Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-       	    }
-	    String sqlStr = "SELECT * FROM login_details WHERE roll_no like ";
-            sqlStr += "'" + name + "'";
-	    sqlStr += "AND pass_hash like '"+encr+"'";
-            //System.out.println("query being done");
-            ResultSet rs=stmt.executeQuery(sqlStr);
-            //System.out.println("queried");
-            myConn.close();
-            //System.out.println("Connection closed");
-	    while(rs.next())
-	    {
-		count++;
-	    }
-            if(count==0) {
-                flag=false;
-            }
-            else{
-                flag=true;
-            }
+            System.out.println("query being done on "+name);
+            ResultSet rs1=stmt.executeQuery("select id from student_main where roll_no like '"+name+"'");
+            System.out.println("queried");
+            
+            System.out.println("Connection closed");
+			int count=0;
+            String garbage="", u_type="";
+			if(rs1.next())
+			{
+				count++;
+				Statement stmt2=myConn.createStatement();
+                obj.id=rs1.getInt(1);//error line if executed forcefully
+                obj.roll_no=name;
+                System.out.println(obj.id);
+                String query2="select * from student_details where student_main_id = '"+obj.id+"'";
+                ResultSet rs2=stmt2.executeQuery(query2);
+                if(rs2.next())
+				{
+                    obj.name=rs2.getString("first_name")+" "+rs2.getString("last_name");
+					System.out.println(obj.name);
+                }
+                System.out.println("id: " + obj.id);
+                obj.flag=true;
+				
+			}
+			myConn.close();
         }
-	catch(Exception e){
-	    System.out.println(e);}
-            return flag;
-        }
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return obj;
+    }
 }
